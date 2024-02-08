b69ca06a56d6cc03f604fa615ae5de685324f8f3
GustavoCremonez <aparecido2332001@gmail.com>
2024-02-07 21:22:45 -0300

    feat: Estilização e controle para adicionar novas notas

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,11 @@ import logo from "./assets/logo.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
+const note = {
+  date: new Date(),
+  content: "lorem"
+}
+
 export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -18,8 +23,7 @@ export function App() {
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={note}/>
       </div>
     </div>
   );
